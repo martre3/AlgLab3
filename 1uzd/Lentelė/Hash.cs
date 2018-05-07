3eb5d7b8020ffcee0b35a573e60cab0e0a718ed9
Tadzius1015 <wtadasw@gmail.com>
2018-05-07 17:54:01 +0300

    1 uzd

---
+++ 1uzd/Lentelė/Hash.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lentelė
+{
+    class Hash
+    {
+        private HashNode[] table;
+        private int index;
+        private int operationsCount;
+        public Hash(int capacity)
+        {
+            table = new HashNode[capacity];
+        }
+        public void Add(string key, int value)
+        {
+            index = FindPlace(key);
+            HashNode element = table[index];
+            if(element == null)
+            {
+                table[index] = new HashNode(key, value);
+            }
+        }
+        private int FindPlace(string key)
+        {
+            int indexx = Hashh(key);
+            int indexxx = indexx;
+            int i = 0;
+            for (int j = 0; j < table.Length; j++)
+            {
+                if (table[indexx] == null || table[indexx].Key.Equals(key))
+                {
+                    return indexx;
+                }
+                i++;
+                indexx = (indexxx + i) % table.Length;
+            }
+            return -1;
+        }
+        private int Hashh(string key)
+        {
+            int hash = key.GetHashCode();
+            return Math.Abs(hash) % table.Length;
+        }
+        public bool ContainsValue(int k, int n, int value)
+        {
+            for (int i = k; i < n; i++)
+            {
+                operationsCount++;
+                if(table[i] != null && table[i].Value == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (HashNode el in table)
+            {
+                if (el != null)
+                {
+                    result.Append(el.ToString());
+                }
+            }
+            return result.ToString();
+        }
+        public int ReturnOperationsCount()
+        {
+            return operationsCount;
+        }
+        public void Clear()
+        {
+            operationsCount = 0;
+        }
+    }
+
+    class HashNode
+    {
+        public string Key;
+        public int Value;
+
+        public HashNode()
+        {
+
+        }
+        public HashNode(string key, int value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+        public override string ToString()
+        {
+            {
+                return String.Format("Raktas: " + Key + " " + "Reikšmė: " + Value);
+            }
+        }
+    }
+}
---
+++ 1uzd/Lentelė/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Lentelė
+{
+    class Program
+    {
+        public const int capacity = 100000;
+        public const int keyLength = 5; // rakto ilgis
+        static Hash table = new Hash(capacity); // sugeneruojama tusčia lentelė
+        static int count = 10000; // sugeneruojamų elementų kiekis
+        static int n = 30; // paieškų kiekis
+        static List<int> rndList = new List<int>(29);
+        static void Main(string[] args)
+        {
+            Reading();
+            ThreadStart ts1 = delegate { Containss(0, capacity / 3, 0); };
+            ThreadStart ts2 = delegate { Containss(capacity / 3, (capacity /3) * 2, 10); };
+            ThreadStart ts3 = delegate { Containss((capacity / 3) * 2, capacity, 20); };
+            Thread th1 = new Thread(ts1);
+            Thread th2 = new Thread(ts2);
+            Thread th3 = new Thread(ts3);
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            th1.Start();
+            th2.Start();
+            th3.Start();
+            watch.Stop();
+           // th1.Abort();
+           // th2.Abort();
+            //th3.Abort();
+            TimeSpan time = watch.Elapsed;
+            Console.WriteLine("Užtruktas laikas gijose: " + time);
+            Console.WriteLine("Įvyko operacijų: " + table.ReturnOperationsCount());
+            Writing(table);
+        }
+        public static void Reading()
+        {
+            string alphabet = "qwertyuiopasdfghjklzxcvbnm0123456789";         
+            Random ats = new Random();
+            int value = 0;
+            for (int i = 0; i < count; i++)
+            {
+                var key = new StringBuilder();
+                for (int j = 0; j < keyLength; j++)
+                {
+                    var text = alphabet[ats.Next(0, alphabet.Length)];
+                    key.Append(text);
+                }
+                value = ats.Next(1, 50);
+                table.Add(key.ToString(), value);
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                rndList.Add(ats.Next(1, 70));  // susikuriu atsitiktinių skaičių aibę
+            }
+        }
+        private static void Containss(int a, int b, int c)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < n / 3; i++)
+            {
+                int sk = rnd.Next(1, 70);
+                bool ats = table.ContainsValue(a, b, rndList.ElementAt(c));
+                c++;
+                //Console.WriteLine(ats);
+            }
+        }
+        public static void Writing(Hash table)
+        {
+            //Console.WriteLine(table.ToString());
+            ///int count = 0;
+            //Console.WriteLine(new string('-', 50));
+            table.Clear();
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            Containss(0, capacity / 3, 0);
+            Containss(capacity / 3, (capacity /3) * 2, 10);
+            Containss((capacity / 3) * 2, capacity, 20);
+            timer.Stop();
+            //count = table.ReturnOperationsCount();
+            //Console.WriteLine("Ar rado reikšmę lentelėje? " + ats);
+            Console.WriteLine("Užtruktas laikas CPU " + timer.Elapsed);
+            Console.WriteLine("Įvyko operacijų: " + table.ReturnOperationsCount());
+        }
+    }
+}
